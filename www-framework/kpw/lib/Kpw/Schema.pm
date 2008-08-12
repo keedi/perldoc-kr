705907e060395d1d4bbc345905f7e4f0c6fd7012
aiatejin <aiatejin@63b10e56-c246-0410-b663-c52a7bc29d04>
2008-08-12 08:22:38 +0000

    KPW2008 :: 스키마 변경 - 소속 추가

--- www-framework/kpw/lib/Kpw/Schema.pm
+++ www-framework/kpw/lib/Kpw/Schema.pm
@@ -8,8 +8,8 @@ use base 'DBIx::Class::Schema';
 __PACKAGE__->load_classes;
 
 
-# Created by DBIx::Class::Schema::Loader v0.04005 @ 2008-08-12 12:31:36
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:3eu+DBrZ3ZfvaZd2JeGrEQ
+# Created by DBIx::Class::Schema::Loader v0.04005 @ 2008-08-12 17:19:51
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:HmbxiRpDjgov1IpdPfgtfQ
 
 
 # You can replace this text with custom content, and it will be preserved on regeneration
