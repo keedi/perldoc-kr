658648e080792a3bd035d4f6521c813954a8057f
keedi.k <keedi.k@63b10e56-c246-0410-b663-c52a7bc29d04>
2008-08-12 16:44:23 +0000

    내용 변화 없음

--- kpw-mailer/mailer.pl
+++ kpw-mailer/mailer.pl
@@ -15,17 +15,17 @@ my %options;
 $options{INCLUDE_PATH} = '/home/saillinux/kpw-mailer/';
 
 my $msg = MIME::Lite::TT::HTML->new(
-                                    From        =>  'korean.perl.workshop@gmail.com',
-                                    To          =>  $params{email},
-                                    Subject     =>  'Korean Perl Workshop 2008 등록 확인 전자우편 #3',
-                                    Template    =>  {
-                                                     text    =>  'kpw-regist.txt.tt',
-                                                     html    =>  'kpw-regist.html.tt',
-                                                    },
-                                    TmplOptions =>  \%options,
-                                    TmplParams  =>  \%params,
-                                    Charset => 'utf8',
-                                    Encoding => '8bit'
-                                   );
+    From     => 'korean.perl.workshop@gmail.com',
+    To       => $params{email},
+    Subject  => 'Korean Perl Workshop 2008 등록 확인 전자우편 #3',
+    Template => {
+        text => 'kpw-regist.txt.tt',
+        html => 'kpw-regist.html.tt',
+    },
+    TmplOptions => \%options,
+    TmplParams  => \%params,
+    Charset     => 'utf8',
+    Encoding    => '8bit'
+);
 
 $msg->send;
