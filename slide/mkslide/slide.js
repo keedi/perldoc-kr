68b53a589e5df9d188f1893c4d00758e32b7aa84
keedi.k <keedi.k@63b10e56-c246-0410-b663-c52a7bc29d04>
2008-08-19 17:47:43 +0000

    *   slide.css     -   어두운 청색 테마 적용 *   mkslide.pl     -   코드 태그에서 제목옵션 사용할 수 있게 함     -   코드 및 목록 태그에서 제목옵션 사용시 p태그로 묶음 *   slide.js     -   사용하지 않는 번역 관련 코드제거

--- slide/mkslide/slide.js
+++ slide/mkslide/slide.js
@@ -1,9 +1,8 @@
 function $(id){ return document.getElementById(id) }
 
-function Slide(frame, screen, slides, pagenum, translate){
+function Slide(frame, screen, slides, pagenum){
     var cur  = 0;
     var max  = 0;
-    var e2j  = new Translator();
     this.init = function(){
         var elems = $(slides).childNodes;
         for (var i = 0; i < elems.length; i++){
@@ -16,23 +15,17 @@ function Slide(frame, screen, slides, pagenum, translate){
     };
     this.go = function(page){
         if (page != undefined) cur = page;
-        if ($(translate).checked) e2j.cancel();
-        e2j.cancel();
         $(screen).innerHTML = $('.slide' + cur).innerHTML;
         if ($(pagenum)) $(pagenum).value = cur;
-        if ($(translate).checked) {
-            var text = $(screen).innerHTML.replace(/\n/g,'');
-            text = text.replace(/<pre>.*<\/pre>/g, '');
-            text = text.replace(/<.*?>/g, '');
-            if (text) e2j.translate(text, $(screen));
-        }
     }
-    this.next = function(){
-        if (++cur >= max) cur = 0;
+    this.next = function(step){
+        step ? cur += step : ++cur;
+        if (cur >= max) cur = 0;
         this.go(cur);
     };
-    this.prev = function(){
-        if (--cur < 0) cur = max - 1;
+    this.prev = function(step){
+        step ? cur -= step : --cur;
+        if (cur < 0) cur = max - 1;
         this.go(cur);
     };
     this.home = function(){
@@ -44,40 +37,7 @@ function Slide(frame, screen, slides, pagenum, translate){
     return this;
 }
 
-function Translator(){
-    var proxy = './e2j.cgi';
-    var xmlhttp;
-    try{
-        xmlhttp = new XMLHttpRequest();
-    }catch(e){
-        xmlhttp = new new ActiveXObject ("Microsoft.XMLHTTP");
-    }
-    this.translate = function(string, element) {
-        if (! xmlhttp) return;        
-        xmlhttp.onreadystatechange = function(){
-            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                var txt = xmlhttp.responseText;
-                element.innerHTML +=
-                    '<p clatxss="translated">('
-                    + txt +
-                    ')</p>';
-            }
-        }
-        xmlhttp.open('POST', proxy , true);
-        xmlhttp.setRequestHeader(
-            "Content-Type", 
-             "application/x-www-form-urlencoded"
-        );
-        xmlhttp.send('q=' + escape(string) + '\n');
-    }
-    this.cancel = function(){
-        if (xmlhttp.readyState == 0 || xmlhttp.readyState == 4) return;
-        xmlhttp.abort();
-    }
-    return this;
-}
-
-var slide = new Slide('frame', 'screen', 'slides', 'pagenum', 'translate');
+var slide = new Slide('frame', 'screen', 'slides', 'pagenum');
 
 window.onload = function(e){
     slide.init()
@@ -86,11 +46,13 @@ window.onload = function(e){
 window.onkeydown = function(e){
     switch(e.keyCode){
         case 35:  // End
-        case 221: // ']'
             { slide.end(); break; }
         case 36:  // Home
-        case 219: // '['
             { slide.home(); break; }
+        case 221: // ']'
+            { slide.next(5); break; }
+        case 219: // '['
+            { slide.prev(5); break; }
         case 37: // Left Arrow
             { slide.prev(); break; }
         case 39: // Right Arrow
