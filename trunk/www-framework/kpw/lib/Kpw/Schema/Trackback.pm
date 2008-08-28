e247e0b04875e6ad26e2a59fa0b0561444fdbac9
aiatejin <aiatejin@63b10e56-c246-0410-b663-c52a7bc29d04>
2008-08-28 05:35:26 +0000

    kpw2008 : trackback 컴포넌트 추가

--- trunk/www-framework/kpw/lib/Kpw/Schema/Trackback.pm
+++ trunk/www-framework/kpw/lib/Kpw/Schema/Trackback.pm
@@ -5,7 +5,7 @@ use warnings;
 
 use base 'DBIx::Class';
 
-__PACKAGE__->load_components("Core");
+__PACKAGE__->load_components("PK::Auto", "+AutoStoreDateTime", "Core");
 __PACKAGE__->table("trackback");
 __PACKAGE__->add_columns(
   "no",
