366d92d82a723330eb9f6c6da09ed69d152441b8
aiatejin <aiatejin@63b10e56-c246-0410-b663-c52a7bc29d04>
2008-08-28 03:17:52 +0000

    kpw2008 : epilogue C 추가

---
+++ trunk/www-framework/kpw/lib/Kpw/C/Epilogue.pm
@@ -0,0 +1,34 @@
+package Kpw::C::Epilogue;
+
+use strict;
+use warnings;
+use base qw( Kpw );
+
+sub do_default {
+    my $self = shift;
+}
+
+1;
+
+=head1 NAME
+
+Kpw::C::Epilogue - Soozy Component
+
+=head1 SYNOPSIS
+
+See L<Kpw>
+
+=head1 DESCRIPTION
+
+Soozy Component
+
+=head1 AUTHOR
+
+Jong-jin Lee
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
